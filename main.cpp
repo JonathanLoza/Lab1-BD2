71083306ad15a5a640662f66ca15f434573b42af
JonathanLoza <lozajnt@gmail.com>
2019-04-03 20:20:01 -0500

    P3 acabada

--- main.cpp
+++ main.cpp
@@ -34,10 +34,14 @@ void add(Alumno *alumno){
 
   int lengthmensualidad=sizeof(alumno->mensualidad);
 
-
   int lengthtotal=sizeof(int)+lengthnombre+sizeof(int)+lengthapellidos+sizeof(int)+lengthciclo+sizeof(int)+lengthmensualidad;
-  //alumno->nmensualidad=(to_string(alumno->mensualidad)).size();*/
+
   ofstream myfile("datos.bin", ios::out | ios::binary | ios::app );
+
+  ofstream registros("registros.bin", ios::out | ios::binary | ios::app);
+  int pos=myfile.tellp();
+
+  registros.write((const char*)&pos,sizeof(int));
 
   myfile.write((const char*)&lengthnombre,sizeof(int));
   myfile.write(alumno->nombre.c_str(),lengthnombre);
@@ -52,6 +56,7 @@ void add(Alumno *alumno){
 }
 
 void load(){
+  cout<<endl<<"Mostrando Todos los registros"<<endl;
   vector<Alumno*> alumnos;
   ifstream file ("datos.bin", ios::in | ios::binary);
   int nbytes;
@@ -60,7 +65,6 @@ void load(){
   Alumno *alumno;
   string str="";
   char* buf;
-  //file.seekg(ios::beg);
   while(!file.eof()){
     alumno=new Alumno();
 
@@ -98,6 +102,49 @@ void load(){
   }
 }
 
+Alumno* read(int pos){
+  Alumno* alumno=new Alumno();
+  string str="";
+  char* buf;
+  int _ciclo;
+  float _mensualidad;
+  int nbytes;
+  ifstream myfile("registros.bin", ios::in | ios::binary);
+  myfile.seekg((pos-1)*4);
+  int nbyte;
+  myfile.read((char*)&nbyte,sizeof(int));
+  myfile.close();
+
+  ifstream file ("datos.bin", ios::in | ios::binary);
+  file.seekg(nbyte);
+  file.read((char*)&nbytes,sizeof(int));
+  buf=new char[nbytes];
+  file.read( buf, nbytes);
+  str.append(buf, nbytes);
+  alumno->nombre=str;
+  str="";
+
+  file.read((char*)&nbytes,sizeof(int));
+  buf=new char[nbytes];
+  file.read( buf, nbytes);
+  str.append(buf, nbytes);
+  alumno->apellidos=str;
+  str="";
+
+  file.read((char*)&nbytes,sizeof(int));
+  file.read((char*)&_ciclo,nbytes);
+  alumno->ciclo=_ciclo;
+
+  file.read((char*)&nbytes,sizeof(float));
+  file.read((char*)&_mensualidad,nbytes);
+  alumno->mensualidad=_mensualidad;
+
+  file.close();
+  cout<<endl;
+  alumno->mostrar();
+  return alumno;
+}
+
 int numero_alumnos(){
   int n_alumnos;
   cout<<"Ingrese # alumnos"<<endl;
@@ -109,6 +156,7 @@ int main() {
   int cantidad=numero_alumnos();
   for(int i=0;i<cantidad;++i){
     Alumno *newAlumno=new Alumno();
+    cout<<endl<<"Ingrese datos"<<endl;
     cout << "nombre: ";
     cin >> newAlumno->nombre;
     cout << "apellidos: ";
@@ -119,9 +167,15 @@ int main() {
     cout << "mensualidad: ";
     cin >> newAlumno->mensualidad;
     add(newAlumno);
-    //load();
   }
+
   load();
+
+  int numero_registro;
+  cout<<endl<<"Ingrese que registro quiere buscar"<<endl;
+  cin>>numero_registro;
+  //No controla que se busque un registro que no exista
+  read(numero_registro);
 
   return 0;
 }
